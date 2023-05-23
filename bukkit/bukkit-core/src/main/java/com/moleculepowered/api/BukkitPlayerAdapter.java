9b53c442c83820bf956c493c1cb870c6f389593f
OMGitzFROST <deltoro.steve98@gmail.com>
2023-05-22 23:23:33 -0700

    Temp

--- bukkit/bukkit-core/src/main/java/com/moleculepowered/api/BukkitPlayerAdapter.java
+++ bukkit/bukkit-core/src/main/java/com/moleculepowered/api/BukkitPlayerAdapter.java
@@ -1,17 +1,10 @@
 package com.moleculepowered.api;
 
 import com.moleculepowered.api.adapter.PlayerAdapter;
-import org.bukkit.entity.Player;
 
 import java.util.Objects;
 
 public class BukkitPlayerAdapter implements PlayerAdapter {
-
-    private final Player player;
-
-    public BukkitPlayerAdapter(Player player) {
-        this.player = player;
-    }
 
     /**
      * Returns the locale of the adapted player
@@ -20,6 +13,6 @@ public class BukkitPlayerAdapter implements PlayerAdapter {
      */
     @Override
     public String getLocale() {
-        return Objects.requireNonNull(NMSBridge.player(player)).getLocale();
+        return Objects.requireNonNull(NMSBridge.player()).getLocale();
     }
 }
