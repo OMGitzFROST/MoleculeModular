ffa9e4bd98241e0fc06242d260559df02c71b386
OMGitzFROST <deltoro.steve98@gmail.com>
2023-07-30 12:23:53 -0700

    Added User Events

---
+++ bukkit/bukkit-platform/src/main/java/com/moleculepowered/platform/bukkit/event/user/UserCreatedEvent.java
@@ -0,0 +1,14 @@
+package com.moleculepowered.platform.bukkit.event.user;
+
+import com.moleculepowered.platform.bukkit.event.AbstractEvent;
+
+/**
+ * Represents an event that is triggered when a new user is created.
+ * This event provides information about the newly created user.
+ *
+ * @author OMGitzFROST
+ */
+public class UserCreatedEvent extends AbstractEvent
+{
+
+}
---
+++ bukkit/bukkit-platform/src/main/java/com/moleculepowered/platform/bukkit/event/user/UserDeletedEvent.java
@@ -0,0 +1,14 @@
+package com.moleculepowered.platform.bukkit.event.user;
+
+import com.moleculepowered.platform.bukkit.event.AbstractEvent;
+
+/**
+ * Represents an event that is triggered when a user is deleted.
+ * This event provides information about the deleted user
+ *
+ * @author OMGitzFROST
+ */
+public class UserDeletedEvent extends AbstractEvent
+{
+
+}
